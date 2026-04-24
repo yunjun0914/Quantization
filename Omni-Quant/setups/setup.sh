8a69743444d98dff4b0847eb4ff0477ed115b2e5
yunjun0914 <yunjun0914@naver.com>
2026-04-24 18:11:19 +0900

    update setup

--- Omni-Quant/setups/setup.sh
+++ Omni-Quant/setups/setup.sh
@@ -7,7 +7,7 @@ source ~/yunjun_env/bin/activate
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 
 # AutoGPTQ 설치
-cd ~/Quantization/Omni-Quant/AutoGPTQ-bugfix && pip install -v .
+cd ~/Quantization/Omni-Quant/AutoGPTQ-bugfix && pip install -v . --no-build-isolation
 
 # OmniQuant 설치
 cd ~/Quantization/Omni-Quant/OmniQuant && pip install -e .
