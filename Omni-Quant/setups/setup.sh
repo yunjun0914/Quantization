6badc07a18205ab35dbe84b4b0bfac445b878682
yunjun0914 <yunjun0914@naver.com>
2026-04-24 17:56:10 +0900

    update setup2

--- Omni-Quant/setups/setup.sh
+++ Omni-Quant/setups/setup.sh
@@ -3,23 +3,23 @@
 # venv 활성화
 source ~/yunjun_env/bin/activate
 
-# PyTorch 먼저 설치 (CUDA 12.1 기준)
+# PyTorch 설치
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 
 # AutoGPTQ 설치
-cd ~/Omni-Quant/AutoGPTQ-bugfix && pip install -v . && cd ~/Omni-Quant
+cd ~/Quantization/Omni-Quant/AutoGPTQ-bugfix && pip install -v .
 
 # OmniQuant 설치
-cd ~/Omni-Quant/OmniQuant && pip install -e . && cd ~/Omni-Quant
+cd ~/Quantization/Omni-Quant/OmniQuant && pip install -e .
 
 # requirements 설치
-pip install -r ~/Omni-Quant/requirements_linux.txt
+pip install -r ~/Quantization/Omni-Quant/requirements_linux.txt
 
 # huggingface_hub 설치
 pip install huggingface_hub
 
 # calibration 데이터 다운로드
-cd ~/Omni-Quant/OmniQuant
+cd ~/Quantization/Omni-Quant/OmniQuant
 git lfs install
 git clone https://huggingface.co/ChenMnZ/act_shifts
 git clone https://huggingface.co/ChenMnZ/act_scales
@@ -27,7 +27,7 @@ git clone https://huggingface.co/ChenMnZ/act_scales
 # OPT-125m 다운로드
 python -c "
 from huggingface_hub import snapshot_download
-snapshot_download(repo_id='facebook/opt-125m', local_dir='./models/opt-125m')
+snapshot_download(repo_id='facebook/opt-125m', local_dir='~/Quantization/Omni-Quant/OmniQuant/models/opt-125m')
 "
 
 echo "Setup done!"
