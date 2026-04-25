995b70ffc663436ed8271b89f6bff5cfde668184
yunjun0914 <yunjun0914@gmail.com>
2026-04-25 17:32:24 +0900

    .

--- Omni-Quant/setups/setup.sh
+++ Omni-Quant/setups/setup.sh
@@ -8,7 +8,7 @@ export CUDA_HOME=/usr/local/cuda
 export PATH=$CUDA_HOME/bin:$PATH
 export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$CUDA_HOME/lib64:$LD_LIBRARY_PATH
 
-source ~/yunjun_env/bin/activate
+source ~/yunjun_omni/bin/activate
 
 python -m pip install --upgrade pip setuptools wheel packaging ninja
 
