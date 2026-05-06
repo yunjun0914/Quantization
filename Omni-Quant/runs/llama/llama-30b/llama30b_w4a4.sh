17b4805b370f7281029fa5c63b82fdf9d28b40ca
yunjun0914 <yunjun0914@naver.com>
2026-05-06 14:32:21 +0900

    update run.sh memory to 120G

--- Omni-Quant/runs/llama/llama-30b/llama30b_w4a4.sh
+++ Omni-Quant/runs/llama/llama-30b/llama30b_w4a4.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=24
-#SBATCH --mem=60G
+#SBATCH --mem=120G
 #SBATCH --time=12:00:00
 #SBATCH --output=/home/yunjun0914/Quantization/Omni-Quant/logs/%x_%j.out
 #SBATCH --error=/home/yunjun0914/Quantization/Omni-Quant/logs/%x_%j.err
