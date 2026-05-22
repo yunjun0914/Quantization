7f1cebb970bb59d89f95f4cc7677507522b5a789
unknown <yunjun0914@naver.com>
2026-05-22 21:52:07 +0900

    run.sh to 3bit

--- Practice/GPTQ/run.sh
+++ Practice/GPTQ/run.sh
@@ -21,7 +21,7 @@ cd ~/Quantization/Practice/GPTQ
 
 python -u main_llama.py \
     --model ~/Quantization/Omni-Quant/OmniQuant/models/llama2-7b \
-    --bits 4 \
+    --bits 2 \
     --rot hadamard \
     --groupsize 128 \
     --compare \
