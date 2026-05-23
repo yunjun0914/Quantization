1858fac43b4c53cfe3710af8d08da803bca25811
unknown <yunjun0914@naver.com>
2026-05-24 03:40:00 +0900

    RotateGPTQ

--- Practice/GPTQ/run.sh
+++ Practice/GPTQ/run.sh
@@ -23,8 +23,8 @@ python -u main_llama.py \
     --model ~/Quantization/Omni-Quant/OmniQuant/models/llama2-7b \
     --bits 2 \
     --rot hadamard \
-    --svd_rank 4
-    --v2\
+    --svd_rank 4 \
+    --v2 \
     --compare \
     --percdamp 0.01 \
     --dev cuda:0 
